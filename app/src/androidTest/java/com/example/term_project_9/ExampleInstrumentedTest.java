7feaae2175d28c17aadab5dfce3da3256f6250b7
hoho00 <jhoya963@gmail.com>
2020-05-20 19:06:20 +0900

    jsp start

--- app/src/androidTest/java/com/example/term_project_9/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/term_project_9/ExampleInstrumentedTest.java
@@ -24,4 +24,5 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.term_project_9", appContext.getPackageName());
     }
+
 }
--- app/src/main/java/com/example/term_project_9/createRoomSettingActivity.java
+++ app/src/main/java/com/example/term_project_9/createRoomSettingActivity.java
@@ -4,13 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class createRoomSettingActivity extends AppCompatActivity {
+    private EditText CategoryEditText;
+    private EditText TermEditText;
+    private EditText MaxNumberEditText;
+    private EditText TitleEditText;
+    private EditText ExplanationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_room_setting);
+        CategoryEditText = (EditText)findViewById(R.id.category_setting);
+        TermEditText = (EditText)findViewById(R.id.term_setting);
+        MaxNumberEditText = (EditText)findViewById(R.id.max_number_setting);
+        TitleEditText = (EditText)findViewById(R.id.title_setting);
+        ExplanationEditText = (EditText)findViewById(R.id.explanation_setting);
+    }
+
+
+    public void settingButton(View view) {
+        String category
     }
 }
